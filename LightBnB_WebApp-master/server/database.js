f79cbde5907a69214746acd05b41df11256c121f
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-14 18:02:23 +0000

    query lightbnb database with getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -67,12 +75,20 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  
+  const queryRes = `
+  SELECT *
+  FROM properties
+  LIMIT $1
+  `;
+
+  pool.query(queryRes, [limit])
+
+  .then(res => {
+      console.log(res.rows)
+    });
 }
+
 exports.getAllProperties = getAllProperties;
 
 
