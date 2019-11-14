b7223e4b57d7d85506cfc146d5d6f5647ed52ce7
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-14 19:27:32 +0000

    return instead of console.log getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,7 +16,7 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
+const getUserWithEmail = function (email) {
   let user;
   for (const userId in users) {
     user = users[userId];
@@ -35,7 +35,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
+const getUserWithId = function (id) {
   return Promise.resolve(users[id]);
 };
 exports.getUserWithId = getUserWithId;
@@ -46,7 +46,7 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
+const addUser = function (user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
@@ -61,7 +61,7 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
+const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
 };
 exports.getAllReservations = getAllReservations;
@@ -75,18 +75,16 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  
+
   const queryRes = `
   SELECT *
   FROM properties
   LIMIT $1
   `;
 
-  pool.query(queryRes, [limit])
+  return pool.query(queryRes, [limit])
 
-    .then(res => {
-      console.log(res.rows);
-    });
+    .then(res => res.rows);
 };
 
 exports.getAllProperties = getAllProperties;
@@ -97,7 +95,7 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+const addProperty = function (property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
